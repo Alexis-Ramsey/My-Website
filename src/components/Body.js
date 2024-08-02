7138827cb70c4d78964f67ba0cb9e3966178e144
Alexis Ramsey <Alexis.ramsey@mscoding.org>
2024-08-02 14:08:03 -0500

    created 2 Bodies

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Body(){
+
+    return(
+        <div className="row">
+            <div className="col-12">I AM BODY </div>
+        </div>
+
+    )
+}
+
+export default Body
---
+++ src/components/Body2.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Body2(){
+
+    return(
+        <div className="row">
+            <div className="col-12">I AM BODY 2</div>
+        </div>
+
+    )
+}
+
+export default Body2
